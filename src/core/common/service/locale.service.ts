67c25f65e967d88fc4e8e398cb103f740f1b8af9
Indra Muliana <indra.ndra26@gmail.com>
2023-03-04 15:58:24 +0700

    resolve HttpResponseInterceptor locale

--- src/core/common/service/locale.service.ts
+++ src/core/common/service/locale.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { I18nContext, I18nService, TranslateOptions } from 'nestjs-i18n';
+import { I18nContext, TranslateOptions } from 'nestjs-i18n';
 
 @Injectable()
 export class LocaleService {
-    constructor(private lang: I18nService) { }
-
     t(key: string, options?: TranslateOptions) {
         const i18n = I18nContext.current();
-        return this.lang.t(key, { ...options, lang: i18n.lang });
+        return i18n.t(key, options);
     }
 }
--- src/core/interceptor/http-response.interceptor.ts
+++ src/core/interceptor/http-response.interceptor.ts
@@ -1,19 +1,19 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor, HttpStatus } from '@nestjs/common';
-import { I18nContext } from 'nestjs-i18n';
 import { map, Observable } from 'rxjs';
+import { LocaleService } from '../common/service/locale.service';
 
 @Injectable()
 export default class HttpResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const code = context.switchToHttp().getResponse().statusCode || HttpStatus.OK;
-        const i18n = I18nContext.current();
+        const locale = new LocaleService();
 
         return next.handle().pipe(
             map((value) => {
                 value = (value) ? value : null
                 return {
                     code,
-                    message: value.message || i18n.t('app.message.success'),
+                    message: value.message || locale.t('app.message.success'),
                     data: value
                 };
             }));
