b0fc4af295d235589382de942374ddd4011213c9
Indra Muliana <indra.ndra26@gmail.com>
2023-03-05 13:13:02 +0700

    configure to db config

--- src/core/config/database.ts
+++ src/core/config/database.ts
@@ -4,5 +4,10 @@ export default registerAs('database', () => ({
     postgres: {
         client: process.env.DB_CLIENT,
         connection_string: process.env.DB_CONNECTION,
+        host: process.env.DB_PG_HOST,
+        port: process.env.DB_PG_PORT,
+        username: process.env.DB_PG_USERNAME,
+        password: process.env.DB_PG_PASSWORD,
+        database: process.env.DB_PG_DATABASE,
     }
 }));
