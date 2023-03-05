547befe4ec62a6b96012c8fefc33a2487d3eefaa
Indra Muliana <indra.ndra26@gmail.com>
2023-03-05 17:49:40 +0700

    Resolve typeorm connection

--- src/core/common/service/typeorm.pg.service.ts
+++ src/core/common/service/typeorm.pg.service.ts
@@ -7,18 +7,7 @@ export class TypeOrmPGConfigService implements TypeOrmOptionsFactory {
 
     constructor(private configService: ConfigService) { }
 
-    createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            name: 'default',
-            type: 'postgres',
-            host: this.configService.get('database.postgres.host', 'localhost'),
-            port: this.configService.get('database.postgres.port', 5432),
-            username: this.configService.get('database.postgres.username'),
-            password: this.configService.get('database.postgres.password'),
-            database: this.configService.get('database.postgres.database'),
-            entities: [],
-            synchronize: false,
-            autoLoadEntities: true,
-        };
+    createTypeOrmOptions(): TypeOrmModuleOptions {        
+        return this.configService.get('database.datasources.default', {});
     }
 }
--- src/core/config/database.ts
+++ src/core/config/database.ts
@@ -9,5 +9,21 @@ export default registerAs('database', () => ({
         username: process.env.DB_PG_USERNAME,
         password: process.env.DB_PG_PASSWORD,
         database: process.env.DB_PG_DATABASE,
+    },
+    
+    datasources: {
+        default: {
+            name: 'default',
+            type: 'postgres',
+            host: process.env.DB_PG_HOST,
+            port: process.env.DB_PG_PORT,
+            username: process.env.DB_PG_USERNAME,
+            password: process.env.DB_PG_PASSWORD,
+            database: process.env.DB_PG_DATABASE,
+            entities: [],
+            synchronize: false,
+            autoLoadEntities: true,
+            migrations: [__dirname + '/../common/database/typeorm/migrations/*.ts'],
+        }
     }
 }));
