e4b6794e0b3434ef7c9e478d397d4144ea76fb65
DESKTOP-FMVAQ3E\Indra QW <indra@qwords.co.id>
2023-03-09 11:04:20 +0700

    serve static assets

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { AppModule } from './app.module';
+import { dirname, join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 import { VersioningType } from '@nestjs/common';
-import HttpResponseInterceptor from './core/interceptor/http-response.interceptor';
 import HttpExceptionFilter from './core/filter/http-exception.filter';
+import HttpResponseInterceptor from './core/interceptor/http-response.interceptor';
+
+const baseDir = dirname(__dirname);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -18,6 +21,11 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new HttpResponseInterceptor());
+  app.useStaticAssets({
+		root: join(`${baseDir}/public`),
+		prefix: '/public/',
+		wildcard: true,
+	});
 
   await app.listen(process.env.APP_PORT || 3000);
   console.log(`Backend server running on: ${await app.getUrl()}`);
