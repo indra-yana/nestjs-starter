ee4a056aa787a8dc7407c2ef32b1810aa8bdd2ed
DESKTOP-FMVAQ3E\Indra QW <indra@qwords.co.id>
2023-03-16 15:36:15 +0700

    resolve readMigrationFile

--- database/typeorm/migration.config.ts
+++ database/typeorm/migration.config.ts
@@ -1,54 +1,38 @@
 import 'dotenv/config';
 import { DataSource } from "typeorm";
-import * as fs from 'fs';
-import * as path from 'path';
-import { CreateUsersTable1678766771204 } from './migrations/1678766771204-Create_Users_Table';
-import { CreateRolesTable1678767760332 } from './migrations/1678767760332-Create_Roles_Table';
-import { CreateFilesTable1678768412234 } from './migrations/1678768412234-Create_Files_Table';
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
 
-function readFile() {
-    const files = fs.readdirSync(path.resolve(`${__dirname}/../common/database/typeorm/migrations`))
+async function readMigrationFile(): Promise<any> {
+    const files = readdirSync(resolve(`${__dirname}/migrations`))
     .map(function(file) {
         return file;
     });
-    
-    async function getFile() {
-        let arr = [];
-        for (let i = 0; i < files.length; i++) {
-            const module = await import(`../common/database/typeorm/migrations/${files[i]}`);                        
-            const value = module[Object.keys(module)[0]];
-    
-            arr.push(value);
-        }
 
-        return arr;
+    const migrationFiles = [];
+    for (let i = 0; i < files.length; i++) {
+        const module = await import(`./migrations/${files[i]}`); 
+        const value = module[Object.keys(module)[0]];
+
+        migrationFiles.push(value);
     }
-
-    // console.log(arr);
-    return getFile();
+    
+    return migrationFiles;
 }
 
+const buildDataSource = async () => { 
+    return new DataSource({
+        type: "postgres",
+        host: process.env.DB_PG_HOST,
+        port: +process.env.DB_PG_PORT || 5432,
+        username: process.env.DB_PG_USERNAME,
+        password: process.env.DB_PG_PASSWORD,
+        database: process.env.DB_PG_DATABASE,
+        migrations: await readMigrationFile(),
+        migrationsTableName: 'typeorm_migrations',
+        logging: false,
+        synchronize: false,
+    });
+}
 
-// console.log(readFile());
-// readFile();
-
-const config = new DataSource({
-    type: "postgres",
-    host: process.env.DB_PG_HOST,
-    port: +process.env.DB_PG_PORT || 5432,
-    username: process.env.DB_PG_USERNAME,
-    password: process.env.DB_PG_PASSWORD,
-    database: process.env.DB_PG_DATABASE,
-    migrations: [
-        // Deprecated: can'nt use string or path migration
-        // path.resolve(`${__dirname}/../common/database/typeorm/migrations/*{.ts,.js}`),
-        CreateUsersTable1678766771204,
-        CreateRolesTable1678767760332,
-        CreateFilesTable1678768412234,
-    ],
-    migrationsTableName: 'typeorm_migrations',
-    logging: false,
-    synchronize: false,
-});
-
-export default config;
+export default buildDataSource();
