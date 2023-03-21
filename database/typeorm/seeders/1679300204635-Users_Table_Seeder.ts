b13e391650402ac93d1fd0d2b56b8cac620194cc
Indra Muliana <indra.ndra26@gmail.com>
2023-03-21 08:08:51 +0700

    install faker and db seeder

--- database/typeorm/seeders/1679300204635-Users_Table_Seeder.ts
+++ database/typeorm/seeders/1679300204635-Users_Table_Seeder.ts
@@ -1,25 +1,49 @@
-import { User } from "../../../src/core/common/database/typeorm/entities/user";
+import { faker } from '@faker-js/faker';
 import { MigrationInterface, QueryRunner } from "typeorm";
-import { nanoid } from "nanoid";
+import { User } from "../../../src/core/common/database/typeorm/entities/user";
 
 export class UsersTableSeeder1679300204635 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // TODO: Handle seeder up and down
-        const user: Partial<User> = new User({
-            id: nanoid(16),
-            name: "root",
-            username: "root",
-            email: "root@comsys.id",
-            password: "secret",
-        })
+        faker.setLocale('id_ID');
+        const users: Partial<User>[] = [];
 
-        const result = await queryRunner.connection
-                            .getRepository(User)
-                            .save(user);        
+        users.push(new User({
+            name: 'root',
+            username: 'root',
+            email: 'root@comsys.id',
+            password: 'secret',
+        }));
+
+        for (let i = 0; i < 50; i++) {
+            const sex = faker.name.sexType();
+            const firstName = faker.name.firstName(sex);
+            const lastName = faker.name.lastName();
+            const username = faker.random.alphaNumeric(8);
+            const email = faker.helpers.unique(faker.internet.email, [
+                firstName,
+                lastName,
+            ]).toLowerCase();
+
+            users.push(new User({
+                name: `${firstName} ${lastName}`,
+                username,
+                email,
+                password: 'secret',
+            }));
+        }
+
+        await queryRunner.connection
+            .getRepository(User)
+            .insert(users);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.connection
+            .createQueryBuilder()
+            .delete()
+            .from(User)
+            .execute()
     }
 
 }
